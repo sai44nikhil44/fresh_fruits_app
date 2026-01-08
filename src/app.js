617af117bf3b0e43e6c15a931dfcfa977c44f0a8
ENT\nikhit2 <nikhit2@medtronic.com>
2026-01-08 07:32:56 +0530

    Initial backend setup with auth and JWT

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const pool = require("./config/db");
+const authRoutes = require("./routes/auth.routes");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.get("/db-test", async (req, res) => {
+try{
+const result = await pool.query("SELECT NOW()");
+  res.json({
+db: "connected",
+time: result.rows[0],
+});
+}
+catch(err){
+console.error(err);
+res.status(500).json({error: "DB Connection failed"});
+}
+});
+
+app.use("/api/auth", authRoutes);
+
+module.exports = app;
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,81 @@
+const pool = require("../config/db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "All fields required" });
+  }
+
+  try {
+    const existing = await pool.query(
+      "SELECT id FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (existing.rows.length > 0) {
+      return res.status(409).json({ message: "Email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      `INSERT INTO users (name, email, password)
+       VALUES ($1, $2, $3)
+       RETURNING id, name, email`,
+      [name, email, hashedPassword]
+    );
+
+    res.status(201).json({
+      message: "User registered",
+      user: result.rows[0],
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Registration failed" });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email & password required" });
+  }
+
+  try {
+    const result = await pool.query(
+      "SELECT id, email, password FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const user = result.rows[0];
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Login failed" });
+  }
+};
+
+module.exports = { register, login };
