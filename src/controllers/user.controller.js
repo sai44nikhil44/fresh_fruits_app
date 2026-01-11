60db978c8998b5a6ebcc663c9bdeed7bc26c6e3f
ENT\nikhit2 <nikhit2@medtronic.com>
2026-01-11 14:03:01 +0530

    added CRUD operations on users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const pool = require("../config/db");
 
-exports.getProfile = async (req, res) => {
+// GET /api/user/me
+exports.getMe = async (req, res) => {
     try {
         const userId = req.user.id;
 
@@ -8,10 +9,52 @@ exports.getProfile = async (req, res) => {
             "SELECT id, name, email, created_at FROM public.users WHERE id = $1",
             [userId]
         );
+
+        if (result.rows.length == 0) {
+            return res.status(404).json({ message: "User not found." });
+        }
         res.json(result.rows[0]);
     }
     catch (err) {
         console.error(err);
         res.status(500).json({ message: "Failed to fetch profile" });
+    }
+};
+
+// PUT /api/user/me
+exports.updateMe = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const { name } = req.body;
+        const result = await pool.query(
+            "UPDATE users SET name = $1 WHERE id = $2 RETURNING id, name, email",
+            [name, userId]
+        );
+
+        res.json({
+            message: "Profile updated",
+            user: result.rows[0]
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Update failed" });
+    }
+};
+
+// DELETE /api/user/me
+exports.deleteMe = async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        await pool.query(
+            "DELETE FROM users WHERE id = $1", [userId]
+        );
+
+        res.json({ message: "Account deleted successfully" });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Delete failed" });
     }
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const authMiddleware = require("../middleware/auth.middleware");
 const userController = require("../controllers/user.controller");
 
-router.get("/me", authMiddleware, userController.getProfile);
+router.get("/me", authMiddleware, userController.getMe);
+router.put("/me", authMiddleware, userController.updateMe);
+router.delete("/me", authMiddleware, userController.deleteMe);
 
 module.exports = router;
