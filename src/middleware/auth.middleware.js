927ec2c76b43ea4ee7e6ab4b002c0cc5cc124030
ENT\nikhit2 <nikhit2@medtronic.com>
2026-01-11 13:26:08 +0530

    added logs

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -7,10 +7,14 @@ module.exports = function (req, res, next) {
         if (!authHeader) {
             return res.status(401).json({ message: "No token provided" });
         }
+        const token = authHeader.split(" ")[1];
+        console.log("token: ", token);
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("decoded: ", decoded);
 
         req.user = decoded;
+        console.log("user decoded: ", req.user);
         next();
     }
     catch (err) {
