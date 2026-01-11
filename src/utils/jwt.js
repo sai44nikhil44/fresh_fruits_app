18e0a3198db928b6587d0e595abd3998a114c90e
ENT\nikhit2 <nikhit2@medtronic.com>
2026-01-11 11:56:01 +0530

    added log to verify

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 function generateToken(payload) {
+    console.log("secret code", process.env.JWT_SECRET);
     return jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN || "7d",
     });
