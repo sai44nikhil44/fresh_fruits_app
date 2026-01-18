d4c55ff1c2875a528214481e50e753e76c244884
ENT\nikhit2 <nikhit2@medtronic.com>
2026-01-18 22:12:35 +0530

    updated controller in route

--- src/routes/admin.payment.routes.js
+++ src/routes/admin.payment.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllPayments } = require("../controllers/admin.payment.controller");
+const controller = require("../controllers/admin.payment.controller");
 const auth = require("../middlewares/auth.middleware");
 const admin = require("../middlewares/admin.middleware");
 
-router.get("/payments", auth, admin, getAllPayments);
+router.get("/payments", auth, admin, controller.getAllPayments);
 
 module.exports = router;
